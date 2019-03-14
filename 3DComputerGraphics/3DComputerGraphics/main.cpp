2e6982cc992da5cc2045fa65540e09947ccb913b
Elisha <elishastaks@gmail.com>
2019-03-14 16:48:19 +1100

    3d grid
    - used gizmos to  create a 3d grid
    - used gizmo clear colour to change scene background

--- 3DComputerGraphics/3DComputerGraphics/main.cpp
+++ 3DComputerGraphics/3DComputerGraphics/main.cpp
@@ -1,6 +1,77 @@
 #include <gl_core_4_4.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/ext.hpp>
+#include <Gizmos.h>
+
+using glm::vec3;
+using glm::vec4;
+using glm::mat4;
+
 
 int main() {
+
+	if (glfwInit() == false)
+		return -1;
+
+	GLFWwindow* window = glfwCreateWindow(1280, 720, "Computer Graphics", nullptr, nullptr);
+
+	if (window == nullptr) {
+		glfwTerminate();
+		return -2;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (ogl_LoadFunctions() == ogl_LOAD_FAILED) {
+		glfwDestroyWindow(window);
+		glfwTerminate();
+	}
+
+	// creating the grid
+	aie::Gizmos::create(65536, 65536, 1000, 1000);
+	mat4 view = glm::lookAt(vec3(10, 10, 10), vec3(0), vec3(0, 1, 0));
+	mat4 projection = glm::perspective(glm::pi<float>() * 0.25f, 16 / 9.f, 0.1f, 1000.f);
+
+	auto major = ogl_GetMajorVersion();
+	auto minor = ogl_GetMinorVersion();
+	printf("GL: %i.%i\n", major, minor);
+
+	while (glfwWindowShouldClose(window) == false && glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS) {
+
+		// COLOR_BUFFER_ BIT: wipes the back buffer colours clean
+		// DEPTH_BUFFER_BIT: clears the distance to the closest pixels (without this openGL will think the image before is still there)
+		glClear( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
+		glClearColor(0.25f, 0.25f, 0.25f, 1);
+		glEnable(GL_DEPTH_TEST); // enables depth buffer
+
+		aie::Gizmos::clear();
+		aie::Gizmos::addTransform(glm::mat4(1));
+
+		vec4 white(1);
+		vec4 black(0, 0, 0, 1);
+
+		for (int i = 0; i < 21; i++)
+		{
+			aie::Gizmos::addLine(vec3(-10 + i, 0, 10),
+				                 vec3(-10 + i, 0, -10),
+				                 i == 10 ? white : black);
+
+			aie::Gizmos::addLine(vec3(10 , 0, -10 + i),
+				                 vec3(-10, 0, -10 + i),
+				                 i == 10 ? white : black);
+		}
+
+
+		aie::Gizmos::draw(projection * view);
+		// updates monitor display
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	aie::Gizmos::destroy();
+	glfwTerminate();
 	return 0;
 }
